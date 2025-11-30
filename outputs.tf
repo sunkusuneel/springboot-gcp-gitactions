d684b5c9550d4ef622d569443b945a99393dd4d7
sunkusuneel <ssuneelk8@gmail.com>
2025-11-30 11:11:52 +0530

    Modify cloud_run_url output with description
    Updated output to use 'uri' and added description.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
 output "cloud_run_url" {
-  value = google_cloud_run_v2_service.springboot.status[0].url
+  description = "The publicly addressable URL of the Cloud Run service."
+  value       = google_cloud_run_v2_service.springboot.uri
 }
+
 
